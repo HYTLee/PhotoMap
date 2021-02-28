259cfb0731f2d89ddaae132ef28ce2f0520a1ba2
Yauheni Hramiashkevich <yauheni.hramiashkevich@apalon.com>
2021-02-28 19:47:30 +0300

    add annotations to not current user positions

--- PhotoMap/Map/MapViewController.swift
+++ PhotoMap/Map/MapViewController.swift
@@ -11,7 +11,7 @@ import AVFoundation
 import CoreLocation
 import RealmSwift
 
-class MapViewController: UIViewController, CLLocationManagerDelegate {
+class MapViewController: UIViewController, CLLocationManagerDelegate, UIGestureRecognizerDelegate {
     
     let mapView = MKMapView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
     var locationManager: CLLocationManager?
@@ -33,12 +33,15 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         self.determineCurrentLocation()
         self.populateDefaultCategories()
         self.updateAnnotations()
+        self.tapHandlerInit()
+
     }
     
     private func setMapView()  {
         mapView.translatesAutoresizingMaskIntoConstraints = false
         self.view.addSubview(mapView)
-        
+        self.mapView.delegate = self
+
         NSLayoutConstraint.activate([
         mapView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 0.0),
         mapView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 0.0),
@@ -58,25 +61,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     @objc func openCamera(){
-        
-        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
-        
-        let openCameraButton = UIAlertAction(title: "Open Camera", style: .default) { (_) in
-            self.showImagePickerController(sourceType: .camera, sourceOfLocation: .curentLocation)
-        }
-        let openGaleryButton = UIAlertAction(title: "Open Galery", style: .default) { [self] (_) in
-            showImagePickerController(sourceType: .photoLibrary, sourceOfLocation: .curentLocation)
-        }
-        
-        let cancel = UIAlertAction(title: "Cancel", style: .cancel) { (_) in
-            print("Canceled")
-        }
-        
-        alertController.addAction(openCameraButton)
-        alertController.addAction(openGaleryButton)
-        alertController.addAction(cancel)
-        
-        self.present(alertController, animated: true, completion: nil)
+        self.openSourceForPhotoAlert(sourceOfLocation: .curentLocation)
     }
     
     private func determineCurrentLocation() {
@@ -140,6 +125,42 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
             mapView.addAnnotation(photoAnnotation)
         }
     }
+    
+    func tapHandlerInit()  {
+        let gestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(handleTap))
+            gestureRecognizer.delegate = self
+            mapView.addGestureRecognizer(gestureRecognizer)
+    }
+    
+    @objc func handleTap(gestureRecognizer: UILongPressGestureRecognizer) {
+        
+        let location = gestureRecognizer.location(in: mapView)
+        let coordinate = mapView.convert(location, toCoordinateFrom: mapView)
+        self.choosedLocation = CLLocation(latitude: coordinate.latitude, longitude: coordinate.longitude)
+        
+        openSourceForPhotoAlert(sourceOfLocation: .mapAnnotation)
+    }
+    
+    
+    func openSourceForPhotoAlert(sourceOfLocation: SourceOfLocation)  {
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        let openCameraButton = UIAlertAction(title: "Open Camera", style: .default) { (_) in
+            self.showImagePickerController(sourceType: .camera, sourceOfLocation: sourceOfLocation)
+        }
+        let openGaleryButton = UIAlertAction(title: "Open Galery", style: .default) { [self] (_) in
+            showImagePickerController(sourceType: .photoLibrary, sourceOfLocation: sourceOfLocation)
+        }
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel) { (_) in
+            print("Canceled")
+        }
+        
+        alertController.addAction(openCameraButton)
+        alertController.addAction(openGaleryButton)
+        alertController.addAction(cancel)
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
 }
 
 extension MapViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
@@ -152,8 +173,8 @@ extension MapViewController: UIImagePickerControllerDelegate, UINavigationContro
         switch sourceOfLocation {
         case .curentLocation:
             choosedLocation = locationManager?.location
-        case .mapAnnotation:
-            choosedLocation = locationManager?.location
+        case .mapAnnotation: break
+            
         }
         present(imagePickerController, animated: true, completion: nil)
     }
@@ -192,5 +213,37 @@ extension MapViewController: UIImagePickerControllerDelegate, UINavigationContro
         self.updateAnnotations()
         self.dismiss(animated: true, completion: nil)
     }
+    
+    
+}
+
+
+extension MapViewController: MKMapViewDelegate {
+    
+     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+//        let annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: "MyMarker")
+//            annotationView.markerTintColor = UIColor.blue
+        
+        guard annotation is PhotoAnnotation else { return nil }
+
+        let identifier = "Photo"
+        
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+    
+        if annotationView == nil {
+
+                annotationView = MKMarkerAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                annotationView?.canShowCallout = true
+            
+                let btn = UIButton(type: .detailDisclosure)
+                annotationView?.rightCalloutAccessoryView = btn
+            } else {
+                annotationView?.annotation = annotation
+            }
+
+            return annotationView
+        }
+    
+
 }
 
--- PhotoMap/Map/PhotoAnnotation.swift
+++ PhotoMap/Map/PhotoAnnotation.swift
@@ -13,10 +13,14 @@ class PhotoAnnotation: NSObject, MKAnnotation {
   var coordinate: CLLocationCoordinate2D
   var photo: Photo?
   var text: String?
+  let title: String?
+  var subtitle: String?
   
   init(coordinate: CLLocationCoordinate2D, text: String, photo: Photo? = nil) {
     self.coordinate = coordinate
     self.text = text
     self.photo = photo
+    self.subtitle = photo?.imageDescription
+    self.title = "Category"
   }
 }
